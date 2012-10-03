32ce4c1d090a56132609d8ca52e75f832fbce548
Pere Orga <gotrunks@gmail.com>
2012-10-03 21:02:22 +0100

    javascript: better endless scrolling

--- index.php
+++ index.php
@@ -37,6 +37,6 @@ if (isset($_GET['n'])) {
 </div>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
-<script src="guia.js"></script>
+<script src="script"></script>
 </body>
 </html>
